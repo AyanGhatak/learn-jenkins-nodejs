82f29d4b3f070664c80348c1ec91af5f159bb32c
Ayan Ghatak <ayan@sumologic.com>
2021-10-04 11:33:44 +0530

    Modify the test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with Hello Ayan', function(done) {
+ it('respond with Hello Soumita', function(done) {
   //navigate to root and check the response is "hello world"
-  request(app).get('/').expect('Hello Ayan', done);
+  request(app).get('/').expect('Hello Soumita', done);
  });
 });
